405be9b11c9eeaeaa8010f7b539331650432c726
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2021-06-03 13:16:04 +0200

    Update proxmox_toolbox.sh

--- proxmox_toolbox.sh
+++ proxmox_toolbox.sh
@@ -15,7 +15,7 @@
 # USAGE
 # You can use this tool either with:
 # git clone https://github.com/Tontonjo/proxmox_toolbox.git
-# bash proxmox_toolbox/proxmox_toolbox/main/proxmox_toolbox.sh
+# bash proxmox_toolbox/proxmox_toolbox.sh
 # OR
 # wget https://raw.githubusercontent.com/Tontonjo/proxmox_toolbox/main/proxmox_toolbox.sh
 # bash proxmox_toolbox/main/proxmox_toolbox.sh
