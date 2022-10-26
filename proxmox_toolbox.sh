5f79d4b6992195e9c21a4cde62b2e8ea74b88c78
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2022-10-26 16:56:16 +0200

    Update proxmox_toolbox.sh

--- proxmox_toolbox.sh
+++ proxmox_toolbox.sh
@@ -80,7 +80,7 @@ version=4.0.1
 if [[ $(id -u) -ne 0 ]] ; then echo "- Please run as root / sudo" ; exit 1 ; fi
 
 # -----------------User variables----------------------
-backupdir="/root/proxmox_config_backups" #trailing slash is mandatory
+backupdir="/root/proxmox_config_backups" # NO trailing slash is mandatory
 backup_content="/etc/ssh/sshd_config /root/.ssh/ /etc/fail2ban/ /etc/systemd/system/*.mount /etc/network/interfaces /etc/sysctl.conf /etc/resolv.conf /etc/hosts /etc/hostname /etc/cron* /etc/aliases /etc/snmp/ /etc/smartd.conf /usr/share/snmp/snmpd.conf /etc/postfix/ /etc/pve/ /etc/lvm/ /etc/modprobe.d/ /var/lib/pve-firewall/ /var/lib/pve-cluster/  /etc/vzdump.conf /etc/ksmtuned.conf /etc/proxmox-backup/"
 
 # ----------------- System variables----------------------
