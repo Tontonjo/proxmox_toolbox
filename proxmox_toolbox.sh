11df0f7aecf5f781b6270bbd48643ed77a324995
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2023-10-10 21:09:48 +0200

    Update proxmox_toolbox.sh

--- proxmox_toolbox.sh
+++ proxmox_toolbox.sh
@@ -342,21 +342,21 @@ main_menu(){
     				# Adding the right backend to fail2ban sshd configuration
 				if ! grep -Fqs "systemd" /etc/fail2ban/jail.d/defaults-debian.conf; then
     				echo "- Backend missing in defaults-debian.conf"
-    				echo "backend = systemd" >> /etc/fail2ban/jail.d/defaults-debian.conf
+    				echo -e "\nbackend = systemd" >> /etc/fail2ban/jail.d/defaults-debian.conf
     				else
 				echo "- Backend already setted-up in defaults-debian.conf"
 				fi
     				if [ -d "$pve_log_folder" ]; then
 	    				if ! grep -Fqs "systemd" /etc/fail2ban/jail.d/proxmox-virtual-environement.conf; then
 	    				echo "- Backend missing in proxmox-virtual-environement.conf"
-	    				echo "backend = systemd" >> /etc/fail2ban/jail.d/proxmox-virtual-environement.conf
+	    				echo -e "\nbackend = systemd" >> /etc/fail2ban/jail.d/proxmox-virtual-environement.conf
 	    				else
 					echo "- Backend already setted-up in proxmox-virtual-environement.conf"
 					fi
      				else
 	        			if ! grep -Fqs "systemd" /etc/fail2ban/jail.d/proxmox-backup-server.conf; then
 	    				echo "- Backend missing in proxmox-backup-server.conf"
-	    				echo "backend = systemd" >> /etc/fail2ban/jail.d/proxmox-backup-server.conf
+	    				echo -e "\nackend = systemd" >> /etc/fail2ban/jail.d/proxmox-backup-server.conf
 	    				else
 					echo "- Backend already setted-up in proxmox-backup-server.conf"
 					fi
